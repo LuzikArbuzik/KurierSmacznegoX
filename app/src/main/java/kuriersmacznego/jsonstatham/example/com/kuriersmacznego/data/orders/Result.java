6d6ec15e69079430b580b0cbbba87dac35e0ae0b
LuzikArbuzik <m.p.w.sadowska@gmail.com>
2017-06-16 21:22:31 +0200

    rozszerzona definicja zamówień

---
+++ app/src/main/java/kuriersmacznego/jsonstatham/example/com/kuriersmacznego/data/orders/Result.java
@@ -0,0 +1,76 @@
+package kuriersmacznego.jsonstatham.example.com.kuriersmacznego.data.orders;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result {
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("dishes")
+    @Expose
+    private List<Dish> dishes = null;
+    @SerializedName("client")
+    @Expose
+    private Client client;
+    @SerializedName("restaurant")
+    @Expose
+    private Restaurant restaurant;
+    @SerializedName("is_taken")
+    @Expose
+    private Boolean isTaken;
+    @SerializedName("is_delivered")
+    @Expose
+    private Boolean isDelivered;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Boolean getIsTaken() {
+        return isTaken;
+    }
+
+    public void setIsTaken(Boolean isTaken) {
+        this.isTaken = isTaken;
+    }
+
+    public Boolean getIsDelivered() {
+        return isDelivered;
+    }
+
+    public void setIsDelivered(Boolean isDelivered) {
+        this.isDelivered = isDelivered;
+    }
+
+}
